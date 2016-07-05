96c1564392a72c0e23eef0f770fa40039629998d
h22roscoe <harry.roscoe14@imperial.ac.uk>
2016-07-06 00:39:05 +0100

    Started up the app by following the angular2 meteor tutorial

---
+++ client/app.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata';
+import { Component } from '@angular/core';
+import { bootstrap } from 'angular2-meteor-auto-bootstrap';
+import { Parties } from '../collections/parties';
+import { Mongo } from 'meteor/mongo';
+
+@Component({
+  selector: 'app',
+  templateUrl: '/client/app.html'
+})
+class FantasyFootball {
+  parties: Mongo.Cursor<Object>;
+
+  constructor() {
+    this.parties = Parties.find();
+  }
+}
+
+bootstrap(FantasyFootball);
